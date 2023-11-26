24c8dcf02c140944305f16ce647706cd823f9d70
scrysoft <admin@scrysoft-studios.com>
2023-11-27 00:01:36 +0100

    Window Events

--- CrappyEngine/src/CrappyEngine/Application.h
+++ CrappyEngine/src/CrappyEngine/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "CrappyEngine/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace CrappyEngine {
@@ -13,7 +15,12 @@ namespace CrappyEngine {
 		virtual ~Application();
 		
 		void Run();
+
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
